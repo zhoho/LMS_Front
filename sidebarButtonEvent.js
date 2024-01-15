c1569f36eac83b64b845bd41bc21c8d2db6c07dd
Zho <chlwlgh1011@naver.com>
2024-01-15 14:37:53 +0900

    feat: css,js 재사용 가능한것 구조 수정

--- sidebarButtonEvent.js
+++ sidebarButtonEvent.js
@@ -41,3 +41,11 @@ document.addEventListener("DOMContentLoaded", function () {
     window.location.href = "course.html"; // 이동하려는 페이지의 URL
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var writeBtn = document.querySelector(".write-btn");
+
+  writeBtn.addEventListener("click", function () {
+    window.location.href = "write.html"; // 이동하려는 페이지의 URL
+  });
+});
